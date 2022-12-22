815347652ff89c10e08e14472250bd0f5a6158d6
Leonardo <leonardocg9492@gmail.com>
2022-12-22 11:55:48 -0600

    setup services

---
+++ commentService/index.ts
@@ -0,0 +1,22 @@
+import express, { Express } from "express";
+import { exit } from "process";
+import { router } from "./src/routers";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use("/", router);
+
+const starService = async () => {
+  try {
+    app.listen(4001, () => {
+      console.log("post service has running on port: 4001");
+    });
+  } catch (error) {
+    console.log(error);
+    exit(1);
+  }
+};
+
+starService();
---
+++ postService/index.ts
@@ -0,0 +1,22 @@
+import express, { Express } from "express";
+import { exit } from "process";
+import { router } from "./src/routers";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use("/", router);
+
+const starService = async () => {
+  try {
+    app.listen(4000, () => {
+      console.log("post service has running on port: 4000");
+    });
+  } catch (error) {
+    console.log(error);
+    exit(1);
+  }
+};
+
+starService();
